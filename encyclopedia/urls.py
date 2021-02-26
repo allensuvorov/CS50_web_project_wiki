c7badc2104f2cc594daf3f78cc14766d24e36747
Allen <allenvs@mail.ru>
2021-02-26 22:58:14 +0300

    added a comment about URLs

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("random", views.random_page, name="random"),
     path("new", views.new, name="new"),
     path("edit", views.edit, name="edit")
+    # To design URLs for an app, you create a Python module informally called a URLconf (URL configuration).
 ]
