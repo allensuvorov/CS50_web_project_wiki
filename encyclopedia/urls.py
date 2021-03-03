df3fa846035b53ab73d2626c05a5fc9dcabbed13
Allen <allenvs@mail.ru>
2021-03-03 22:15:23 +0300

    now links work with wiki/

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:entry>/", views.entry, name="entry"),
+    path("wiki/", views.index, name="index"),
+    path("wiki/<str:entry>/", views.entry, name="entry"),
     path("search", views.search, name="search"),
     path("random", views.random_page, name="random"),
     path("new", views.new, name="new"),
-    path("<str:title>/edit", views.edit, name="edit")
+    path("wiki/<str:title>/edit", views.edit, name="edit")
     # To design URLs for an app, you create a Python module informally called a URLconf (URL configuration).
 ]
