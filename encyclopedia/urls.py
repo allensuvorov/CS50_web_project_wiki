2f7d5a66477246e690302fbab87b3d796b98c2fd
Allen <allenvs@mail.ru>
2021-03-16 23:32:26 +0300

    path change wiki/new

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("wiki/<str:entry>/", views.entry, name="entry"),
     path("wiki/search", views.search, name="search"),
     path("wiki/random", views.random_page, name="random"), # edit makes Request URL:	http://127.0.0.1:8000/edit
-    path("new", views.new, name="new"),
+    path("wiki/new", views.new, name="new"),
     path("wiki/<str:title>/edit", views.edit, name="edit")
 ]
