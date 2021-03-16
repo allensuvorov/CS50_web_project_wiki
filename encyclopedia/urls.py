102f0a2ac657a14f5c0c6839e49c5e5257f550a6
Allen <allenvs@mail.ru>
2021-03-16 23:11:30 +0300

    added wiki/ to search path for search result links to works

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("wiki/", views.index, name="index"),
     path("wiki/<str:entry>/", views.entry, name="entry"),
-    path("search", views.search, name="search"),
+    path("wiki/search", views.search, name="search"),
     path("random", views.random_page, name="random"), # edit makes Request URL:	http://127.0.0.1:8000/edit
     path("new", views.new, name="new"),
     path("wiki/<str:title>/edit", views.edit, name="edit")
